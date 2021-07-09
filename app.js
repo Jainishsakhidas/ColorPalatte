b64ac6fd6bb75da31b4d7416fec6b0617e320c56
Jainish Sakhidas <jainishsakhidas@gmail.com>
2021-07-09 18:20:11 +0530

    Colorized Sliders

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ function randomColors() {
 
     //Check for contrast
     checkTextContrast(randomColor, hexText);
+
+    //Initial Colorize Sliders
+    const color = chroma(randomColor);
+    const sliders = div.querySelectorAll(".sliders input");
+    const hue = sliders[0];
+    const brightness = sliders[1];
+    const saturation = sliders[2];
+
+    colorizeSliders(color, hue, brightness, saturation);
   });
 }
 
@@ -42,6 +51,28 @@ function checkTextContrast(color, text) {
   } else {
     text.style.color = "white";
   }
+}
+
+function colorizeSliders(color, hue, brightness, saturation) {
+  //Scale Saturation
+  const noSat = color.set("hsl.s", 0);
+  const fullSat = color.set("hsl.s", 1);
+  const scaleSat = chroma.scale([noSat, color, fullSat]);
+
+  //Scale Brightness
+  const midBright = color.set("hsl.l", 0.5);
+  const scaleBright = chroma.scale(["black", midBright, "white"]);
+
+  //Update Input colors
+  saturation.style.backgroundImage = `linear-gradient(to right,${scaleSat(
+    0
+  )}, ${scaleSat(1)})`;
+
+  brightness.style.backgroundImage = `linear-gradient(to right, ${scaleBright(
+    0
+  )} , ${scaleBright(0.5)} ,${scaleBright(1)})`;
+
+  hue.style.backgroundImage = `linear-gradient(to right, rgb(2204,75,75),rgb(204,204,75),rgb(75,204,75),rgb(75,204,204),rgb(75,75,204),rgb(204,75,204),rgb(204,75,75))`;
 }
 
 randomColors();
